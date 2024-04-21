b7d1118518ae0d05e042118f6b2b0783adce780b
AG16672 <garantiardmsinic@igac.gov.co>
2024-04-21 18:09:12 -0500

    first commit

---
+++ src/main/java/com/magilaTI/msSpotify/MsSpotifyApplication.java
@@ -0,0 +1,13 @@
+package com.magilaTI.msSpotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magilaTI/msSpotify/MsSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magilaTI.msSpotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
